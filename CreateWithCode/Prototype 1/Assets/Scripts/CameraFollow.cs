e121e586d8bb4fd81a7b58d1c4729d1cde1641c2
Carson Oyler <carson.o906@stu.nebo.edu>
2019-09-20 10:31:29 -0600

    Started on the challenge

--- CreateWithCode/Prototype 1/Assets/Scripts/CameraFollow.cs
+++ CreateWithCode/Prototype 1/Assets/Scripts/CameraFollow.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 5, -7);
-
-   
+    private Vector3 offset = new Vector3(0, 5, -10);
+    
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + offset; 
+        transform.position = player.transform.position + offset;  
     }
 }
