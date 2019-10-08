994628af87603e35e19070bb46b3d82a9caed9fd
Carson Oyler <carson.o906@stu.nebo.edu>
2019-10-08 10:29:21 -0600

    Started coding to make the player move

---
+++ CreateWithCode/Prototype 2/Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+public float horizontalInput;
+public float speed = 15f;
+public float xRange = 20.0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    if(transform.position.x < -xRange)
+    {
+        transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+    }
+    
+    if(transform.position.x > xRange)
+    {
+        transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+    }
+    
+        //i(transform.position.x < -10)
+        //{
+        //transform.position = new Vector3()
+        //}
+        horizontalInput = Input.GetAxis("Horizontal");
+        
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+        
+    }
+}
