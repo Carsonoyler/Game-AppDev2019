c82d82e2b46fef4a0879ffce0d6d2556c7066190
Carson Oyler <carson.o906@stu.nebo.edu>
2019-11-15 10:28:19 -0700

    Added death animation
    Made jumping animation smooth and added death animation

--- CreateWithCode/Prototype3/Assets/Scripts/SpawnManager.cs
+++ CreateWithCode/Prototype3/Assets/Scripts/SpawnManager.cs
@@ -10,6 +10,7 @@ public class SpawnManager : MonoBehaviour
     private float startDelay = 2;
     private float repeatRate = 2;
     private PlayerController playerControllerScript;
+    
     // Start is called before the first frame update
     void Start()
     {
