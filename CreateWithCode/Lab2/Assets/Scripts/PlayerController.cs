d2ff44f6c776f6803d130194f46df4f47c7963b6
Carson Oyler <carson.o906@stu.nebo.edu>
2019-12-04 10:29:03 -0700

    Finished lab 3
    made the player move within the screen

--- CreateWithCode/Lab2/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Lab2/Assets/Scripts/PlayerController.cs
@@ -8,15 +8,28 @@ public class PlayerController : MonoBehaviour
     private float speed = 10.0f;
     private Rigidbody playerRb;
     public float horizontalInput;
+    public float xRange = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Adds rigidBody to the player
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Makes the player move ledt and right, but not go off screen
+         if(transform.position.x < -8)
+        {
+            transform.position = new Vector3(-8, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > 18)
+        {
+            transform.position = new Vector3(18, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
     }
 }
